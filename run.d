03e01375fddef6c8d314415b41072a7b6299caf5
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2019-06-23 02:33:50 +0300

    Added versioning for 32 bit or 64 bit systems

--- run.d
+++ run.d
@@ -25,13 +25,27 @@ void main(string[] args)
 
     if (args[1] == "tests")
     {
-        compile = "rdmd -m64 -O -inline --build-only tests.d Dmemset.d";
-        execute = "./tests a";
+        version (x86_64)
+        {
+            compile = "rdmd -m64 -O -inline --build-only tests.d Dmemset.d";
+        }
+        else
+        {
+            compile = "rdmd -O -inline --build-only tests.d Dmemset.d";
+        }
+        execute = "./tests";
     }
     else
     {
-        compile = "rdmd -m64 -O -inline --build-only benchmarks.d Dmemset.d";
-        execute = "./benchmarks a";
+        version (x86_64)
+        {
+            compile = "rdmd -m64 -O -inline --build-only benchmarks.d Dmemset.d";
+        }
+        else
+        {
+            compile = "rdmd -O -inline --build-only benchmarks.d Dmemset.d";
+        }
+        execute = "./benchmarks";
     }
     if(run(compile) != 0)
     {
